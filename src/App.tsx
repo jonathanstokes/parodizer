d6bfa7e15ccf576cefa8dbc8bed016bd39641cff
jonathanstokes <stokes@casetext.com>
2020-07-28 17:10:26 -0500

    Set up eslint and prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import {Button} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 import logo from './logo.svg';
 import './App.css';
@@ -9,18 +9,20 @@ import {Job} from "./client-and-server/lyric-list-service-types";
 
 function App() {
   const [searchTerms, setSearchTerms] = useState<string>('');
+  const [isSearching, setIsSearching] = useState<boolean>(false);
   const onSearch = async (query: string) => {
     const result = await axios.post<Job>(`/api/search`, {q: query});
     if (result.status === 202) {
+        setIsSearching(true);
       const job = result.data;
     }
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="parodizer-app">
+      <header className="app-header">
         Parodizer
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <img src={logo} className="logo" alt="logo" />
       </header>
       <p>
         Enter a word or words to rhyme in a popular song:
